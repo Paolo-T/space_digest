85c9d6d3f838a0eb2c028ef73369f1e2f3272932
Paolo Todde <ptodde@brandwatch.com>
2020-04-08 11:43:24 +0100

    Design Update

--- client/src/pages/page__asteroids.js
+++ client/src/pages/page__asteroids.js
@@ -3,7 +3,7 @@ import Asteroids from "../components/Asteroids";
 
 function page__asteroids() {
     return (
-        <div className="container mx-auto pr-30 py-24 text-left">
+        <div className="container mx-auto pr-30 pt-12 mb-24 text-left">
             <div className="w-9/12 mx-auto">
                 <h1>Asteroids</h1>
                 <Asteroids />
