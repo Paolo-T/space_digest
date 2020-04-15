2242f345a9dba069e339b3188bf50f48b62bf8bf
Ewen Cluley <ewen@brandwatch.com>
2020-04-15 16:05:22 +0100

    changed api port

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 const http = require("http");
 const app  = require("./backend");
 
-const PORT = process.env.PORT || 5000;
+const PORT = /*process.env.PORT || */5000;
 
 const server = http.createServer(app);
 
